9b470c4e0e4acbd0a8cc2f773de0db2f3306e218
john <johnH@hexatone.net>
2024-02-05 10:42:27 +0200

    paypal live

--- paypal_standard_checkout_in_php/config.php
+++ paypal_standard_checkout_in_php/config.php
@@ -1,25 +1,25 @@
 <?php 
  
 // Product Details 
-$itemNumber = "BET20"; 
+$itemNumber = "Ticket"; 
 $itemName = "Enter Pool"; 
-$itemPrice = 0;  
-$currency = "AUD"; 
- 
+$itemPrice = 20;  
+$currency = "AUD";
+
 /* PayPal REST API configuration 
  * You can generate API credentials from the PayPal developer panel. 
  * See your keys here: https://developer.paypal.com/dashboard/ 
- */ 
-define('PAYPAL_SANDBOX', TRUE); //TRUE=Sandbox | FALSE=Production 
+ */
+define('PAYPAL_ID', 'john.horton86@gmail.com'); //Real ID
+
+define('PAYPAL_SANDBOX', FALSE); //TRUE=Sandbox | FALSE=Production 
 define('PAYPAL_SANDBOX_CLIENT_ID', 'AdqP5SMZsMBx8kiJsiUlUnfBoJSrk18wnCwim1oVOvvLjMWQup8z3rEpDPLMPb-4pjwvU_gQHkZZ_Ak7'); 
 define('PAYPAL_SANDBOX_CLIENT_SECRET', 'EDr5aIFQBMEPsjC8aE9oTz0u9vJhuFUa58o0Om628x8jjHQq7Wti6kCBpy9gRXP7odmXXbXea-38VNOd'); 
-define('PAYPAL_PROD_CLIENT_ID', 'Insert_Live_PayPal_Client_ID_Here'); 
-define('PAYPAL_PROD_CLIENT_SECRET', 'Insert_Live_PayPal_Secret_Key_Here'); 
+define('PAYPAL_PROD_CLIENT_ID', 'Aczoz-VmxG-gYeA0F6uv0P0RboZn2ZMTY_NLyW5jqu-JPdeGGIgrW7yEsvPOJKdC4Vp7f8WT_Ehm65JJ'); 
+define('PAYPAL_PROD_CLIENT_SECRET', 'EJ8Uabe2rn3KcX8hhE13q6yMluH1sGAD3DnJm0BskkPNXNIKmso-tA2D7Z8BW2oqsER71cdeYcn5Ju04'); 
   
 // Database configuration  
-define('DB_HOST', 'localhost');  
-define('DB_USERNAME', 'john');  
-define('DB_PASSWORD', 'john');  
-define('DB_NAME', 'afl'); 
- 
-?>
+define('DB_HOST', '13.49.223.11');  
+define('DB_USERNAME', 'aflpools');  
+define('DB_PASSWORD', 'V4513john');  
+define('DB_NAME', 'afl');
