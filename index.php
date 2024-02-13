e7658ad28f9f4790a870fc9a78e2e724d43dcca9
john <johnH@hexatone.net>
2024-02-13 08:24:45 +0200

    stripe

--- index.php
+++ index.php
@@ -100,21 +100,14 @@
                 </div>
             </div>
         </div>
-        <div id="stripebox" class="container stripecontainer modal modal-content" style="display: none; border-radius: 0%; margin-top: 5rem; padding: 0; height:auto; z-index: 4; margin-top: 100px;">
-            <div id="checkout" style="position: reletive; overflow-y:scroll; height: 100%; margin-top: 200px;">
-                <!-- Checkout will insert the payment form here -->
-            </div>
-            <button onclick="cancelDeposit();" style="width: 100%;">Cancel Deposit</button>
-        </div>
+
         <div id="afldata" class="main-content">
-            <div id="stripebox" class="container stripecontainer modal modal-content"
-                style="display: none; border-radius: 0%; margin-top: 5rem; padding: 0; height:auto; z-index: 4; margin-top: 100px;">
+            <div id="stripebox" class="container stripecontainer modal modal-content" style="display: none; border-radius: 0%; margin-top: 5rem; padding: 0; height:auto; z-index: 4; margin-top: 200px; position: relative; height: 100%; overflow-y: scroll;">
                 <div id="checkout">
                     <!-- Checkout will insert the payment form here -->
                 </div>
                 <button onclick="cancelDeposit();" style="width: 100%;">Cancel Deposit</button>
             </div>
-
             <div id="prizepool" class="colcontainer" style="min-height: 65px">
                 <img src="./images/saco.svg" width=60 height=60 style="margin-left: 30%; position: relative;" />
                 <div class="row">
