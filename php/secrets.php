83ebe41a44a2b37bac13898f5cb0270de15b1080
JohnWHorton <156651315+JohnWHorton@users.noreply.github.com>
2024-02-17 13:24:10 +0200

    Delete php/secrets.php
    now in gitignore

--- php/secrets.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is your test secret API key.
-// $stripeSecretKey = 'sk_test_51OBZslDol7H5bJPdirYEJGO1wtq7Q4RfBTaWUbzv25aUmp2fNmk3mHi9rf561PoujuhqVlSu5AuHqMN2Xaf4ieSW008pcdVLKp';
-//live
-$stripeSecretKey = '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';
