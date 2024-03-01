0593b975187c445ff0bdd28defea516352f70d99
JohnWHorton <156651315+JohnWHorton@users.noreply.github.com>
2024-03-01 19:43:13 +0200

    vid

--- js/aflpools.js
+++ js/aflpools.js
@@ -768,7 +768,7 @@ function showDemoVideo() {
   <div style="width: 100%; min-width: 400px; max-width: 800px;">
     <div style="position: relative; width: 100%; overflow: hidden; padding-top: 56.25%;">
       <p>
-        <iframe style="position: absolute; top: 0; left: 0; right: 0; width: 100%; height: 100%; border: none; overflow: hidden;" src="https://www.youtube.com/embed/e1OeU1qq7KA?si=0o03iEFexIY0vq4I" width="560" height="315" allowfullscreen="allowfullscreen" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe></p>
+        <iframe style="position: absolute; top: 0; left: 0; right: 0; width: 100%; height: 100%; border: none; overflow: hidden;" src="https://www.youtube.com/embed/e1OeU1qq7KA?si=shCVFDEcvDT4uRJ7" width="560" height="315" allowfullscreen="allowfullscreen" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe></p>
     </div>
   </div>`;
 
